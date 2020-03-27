76ff54cb36bfa9372a562dac0dbdeacdc96fae18
Michael Mennis <mmennis@gmail.com>
2020-03-27 18:26:05 +0000

    Adding faker and vet creation

--- demo/settings.py
+++ demo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'coverage',
+    'faker',
     'petclinic.apps.PetclinicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- petclinic/management/commands/populate_db.py
+++ petclinic/management/commands/populate_db.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from petclinic.models import Owner, Pet, PetType, Vet, Specialty, Visit
+from faker import Faker
+fake = Faker()
+import random
 
 class Command(BaseCommand):
     help = 'Populates the petclinic database with fake sample data for development work'
@@ -29,13 +32,16 @@ class Command(BaseCommand):
         self.clean_up_db()
         self.create_pet_types()
         self.create_specialties()
+        self.create_vets()
         self.stdout.write(self.style.SUCCESS('PetType count: ' + str(PetType.objects.count())))
         self.stdout.write(self.style.SUCCESS('Specialty count: ' + str(Specialty.objects.count())))
+        self.stdout.write(self.style.SUCCESS('Vet count: ' + str(Vet.objects.count())))
 
 
     def clean_up_db(self):
         PetType.objects.all().delete()
         Specialty.objects.all().delete()
+        Vet.objects.all().delete()
 
     def create_pet_types(self):
         pet_type_list = ['bird','cat','dog','fish','hamster','horse','iguana',        
@@ -53,3 +59,19 @@ class Command(BaseCommand):
         for sp in specialty_list:
             specialty = Specialty.objects.create(name=sp)
             self.specialties.append(specialty)
+
+    def create_vets(self):
+        self.vets = []
+        for i in range(0, self.vet_count):
+            specialty = self.specialties[random.randrange(len(self.specialties))]
+            vet = Vet.objects.create(
+                email=fake.email(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                street_address=fake.street_address(),
+                city=fake.city(),
+                state=fake.state_abbr(),
+                telephone=fake.phone_number(),
+                specialty=specialty
+            )
+            self.vets.append(vet)
